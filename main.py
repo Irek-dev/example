7da5ea78a37f87026209b959b768b9f5a77f4551
Ирек <sakirov436@gmail.com>
2022-02-10 17:46:57 +0300

    Я добавил метод возраст.

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 def HI(name):
     print("Hello " + name)
 
+def age(age):
+    if age < 18:
+        print("Hello little baby")
+    else:
+        print("Yo bro")
 
-name = "world"
+age = 15
+name = "Irek"
 HI(name)
+age(age)
